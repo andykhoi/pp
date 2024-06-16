8b7a8cd2ecd96110bb7bab873c772f4b832ea3e8
andykhoi <akitran@ucdavis.edu>
2024-06-15 22:25:05 -0700

    added fix to spacer

--- src/app/for/onestepgps/page.tsx
+++ src/app/for/onestepgps/page.tsx
@@ -70,7 +70,7 @@ export default function OneStepGPS() {
 				poster='/steep_poster.png'
 				link='/for/onestepgps/dev-lionsgate'
 			/>
-			<Spacer height='69px' breakpointHeight='28px' />
+			<Spacer height='12px' breakpointHeight='28px' />
 			<VideoPreview 
 				blurDataURL='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAF3SURBVHgBDY/NThNRAEbPbWc6t0LbSatBQxQTLWisf4gmLt0YRRM3yhu4dOXCuDPxYUzcEZcmwIIEQgINJRBK+GmBQAttp52WdMrcO8Mk3/J8Jzni54/voTmc46xxzvW0xasnEzyefE2712Pga3QIvtIYRr9DvlBg5u0UE3dHEYTUqut0WyGNC0X7wkepAOP51CTTb54ihIBAR7skLUHWlmnu1HHbAw7rDQzHI3oojJhAa5+IRps2+13JWrFMx2nQcj2MHC6Dfo+YZUaQoLhd5WzzP7cKHzhRi5TKdYx4jPi7cX5pIdk9dVksHUS2IZqXcY5qLWRS4noKU0pi+eQu3a7DcUdx514et+Pw5+8sO3sVVoslbBy+fXqEkcjY3HjxEdtXZDMpqpUKCStJeWWelw9G+Dz9BTtlRg2ZhwzrJlsnLjlrlLW5WcasNjNf3zOev48wLULlIcr/fofyWoqFpQ36ns/trGBo7BlBeoSbqQTZ0CUIAq4A3ESflV4rj00AAAAASUVORK5CYII='
 				src='/steep.mp4'
